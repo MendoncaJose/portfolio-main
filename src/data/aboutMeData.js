76db150260d3e3103c4c6104c60264d990a7a17c
MendoncaJose <mendoncajose7@gmail.com>
2023-04-15 12:05:43 +0100

    Update Bio

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,14 @@
 export const aboutMeData = [
 	{
 		id: 1,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
+		bio: 'Hello, my name is José Mendonça and I am someone who recently specialized in programming after completing a Technological Specialization Course (CET). I am passionate about technology and excited to pursue a career in this field.',
 	},
 	{
 		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+		bio: 'During the CET, I acquired programming skills and learned fundamental concepts such as variables, conditional and repetition structures, functions, and libraries. I was able to apply this knowledge to create small programs and develop my problem-solving skills.',
+	},
+	{
+		id: 3,
+		bio: 'I am a curious and determined person who is always looking to learn more and improve my skills. I am constantly seeking opportunities to apply my programming knowledge in a challenging and collaborative work environment where I can grow and contribute to innovative projects.',
 	},
 ];
